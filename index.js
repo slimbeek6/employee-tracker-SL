9deee0f7176248cf372b9d1c007ba6e68ee35c87
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-31 13:16:24 -0800

    create initial overall inquirer

--- index.js
+++ index.js
@@ -20,6 +20,57 @@ connection.connect(function(err) {
   runCMS();
 });
 
+const cmsoptions = ["View All Employees", "View All Employees by Department", "View All Employees by Manager","Add Employee","Remove Employee", "Update Employee Role","Update Employee Manager","View All Roles", "Add Role","Remove Role","Exit"];
+
 function CMS () {
     inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "",
+        choices: cmsoptions
+    })
+    .then(function (answer) {
+        switch (answer.action) {
+            case "View All Employees":
+                showEmployees();
+                break;
+        
+            case "View All Employees by Department":
+                searchEmplDept();
+                break;
+
+            case "View All Employees by Manager":
+                searchEmplMan();
+                break;
+            
+            case "Add Employee":
+                addEmpl();
+                break;
+
+            case "Remove Employee":
+                removeEmpl();
+                break;
+
+            case "Update Employee Role":
+                updateEmplRole();
+                break;
+            
+            case "Update Employee Manager":
+                updateEmplMan();
+                break;
+            
+            case "View All Roles":
+                showRoles();
+                break;
+            
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Remove Role":
+                removeRole();
+                break;
+        }    
+   });
 }
