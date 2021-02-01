d1eea3f4b728dc5ab3778020ed626a45ef2cc6d0
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-31 18:39:12 -0800

    created the add and delete an employee functions

--- index.js
+++ index.js
@@ -137,3 +137,113 @@ function searchEmplMan () {
         });
     });    
 }
+
+var rolesArr = [];
+
+function addEmpl () {
+    connection.query("SELECT * FROM managers", function(err, res) {
+        for (var i=0; i < res.length; i++) {
+            // console.log(res);
+            var obj = {name: res[i].name, id: res[i].employee_id};
+            managerArr.push(obj);
+        }
+
+        var mgrArr = [];
+        for (var j = 0; j < managerArr.length; j++) {
+            mgrArr.push(managerArr[j].name);
+        }
+
+        connection.query("SELECT title FROM roles;", function(err, res) {
+            for (var i=0; i < res.length; i++) {
+                rolesArr.push(res[i].title);
+            }
+            // console.log(managerArr);
+
+            inquirer
+            .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    name: "role",
+                    type: "list",
+                    message: "What is the employee's role?",
+                    choices: rolesArr
+                },
+                {
+                    name: "department",
+                    type: "list",
+                    message: "What is the employee's department?",
+                    choices: departmentArr
+                },
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "What is the employee's first name?",
+                    choices: mgrArr
+                },
+            ])
+            .then(function(answer) {
+                var first_name = answer.first_name;
+                var last_name = answer.last_name;
+                var role_id = rolesArr.indexOf(answer.role) +1;
+                var department_id = departmentArr.indexOf(answer.department) +1;
+                var mgr1 = mgrArr.indexOf(answer.manager);
+                // console.log(managerArr);
+                
+                var manager_id = managerArr[mgr1].id;
+                // console.log(managerId);
+                connection.query("INSERT INTO employees SET ?;", { first_name, last_name, role_id, department_id, manager_id} , function(err) {
+                    if (err) throw err;
+                    console.log("Your employee was saved successfully!");
+                    console.log("What would you like to do?");
+                    runCMS();
+                });
+            });
+        });
+    });
+}
+
+var emplArr = [];
+var emplNames = [];
+
+function removeEmpl () {
+    connection.query("Select first_name, last_name, id FROM employees", function(err, res) {
+        for (var i =0; i < res.length; i++) {
+            var fullName = res[i].first_name + " " + res[i].last_name;
+            var obj = {fullName: fullName, id: res[i].id}
+            emplArr.push(obj);
+            emplNames.push(fullName);
+        }        
+
+        // console.log(emplArr);
+
+        inquirer
+        .prompt({
+            name: "employee",
+            type: "list",
+            message: "Which employee do you want to delete?",
+            choices: emplNames
+        })
+        .then(function(answer) {
+            var emplind = emplNames.indexOf(answer.employee);
+            var emplname = answer.employee;
+            var emplid = emplArr[emplind].id;
+
+            connection.query("DELETE FROM employees WHERE ?", {id: emplid}, function(err) {
+                if (err) throw err;
+                console.log("Employee "+ emplname + " has been removed");
+                runCMS();
+            });
+        });
+    });  
+}
+
+
